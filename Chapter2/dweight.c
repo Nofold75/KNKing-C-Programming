7d9da1d4c620206bec9fe6c1dc96acf64783e006
Nofold75 <fadel.mehdi@gmail.com>
2026-09-20 20:52:50 +0200

    added exo1.c & refactoring dweight.c

--- Chapter2/dweight.c
+++ Chapter2/dweight.c
@@ -9,11 +9,11 @@ Author: Mehdi F.
 
 int main(void) 
 {
-    int height = 8, length = 12, width = 10, volume = height*length*width, weight = (volume + 165)/166; 
+    int height = 8, length = 12, width = 10, volume = height*length*width; 
     
     printf("Dimensions: %dx%dx%d\n", length, width, height);
     printf("Volume (cubic inches): %d\n", volume);
-    printf("Dimensional weight (pounds): %d\n", weight);
+    printf("Dimensional weight (pounds): %d\n", (volume + 165)/166);
     
     return 0;
 }
---
+++ Chapter2/exo1.c
@@ -0,0 +1,28 @@
+/* 
+Name: exo1.c
+Purpose: Program that uses print to display the following picture on the screen:
+
+         *
+        *
+       *
+ *    *
+  *  *
+   *
+
+Date: 09/20/2026 
+Author: Mehdi F.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("         *\n");
+    printf("        *\n");
+    printf("       *\n");
+    printf(" *    *\n");
+    printf("  *  *\n");
+    printf("   *\n");
+
+    return 0;
+}
