f1f2d549c125a6139a9b864b1262382ce91d218f
Nofold75 <fadel.mehdi@gmail.com>
2026-09-20 19:52:11 +0200

    added celsius.c

---
+++ Chapter2/celsius.c
@@ -0,0 +1,23 @@
+/* 
+Name: dweight.c
+Purpose: Convert a Fahrenheit temperature to Celsius.
+Date: 09/20/2026 
+Author: Mehdi F.
+*/
+
+#include <stdio.h>
+
+#define FREEZING_PT 32.0f
+#define SCALE_FACTOR (5.0f / 9.0f)
+
+int main(void)
+{
+    float fahrenheit, celsius;
+
+    printf("Enter Fahrenheit temperature: ");
+    scanf("%f", &fahrenheit);
+    celsius = (fahrenheit - FREEZING_PT) * SCALE_FACTOR;
+    printf("Celsius equivalent: %.1f\n", celsius);
+
+    return 0;
+}
