0f1e547130d7b4daaabfa44d618688b36bdede2a
Nofold75 <fadel.mehdi@gmail.com>
2026-09-21 20:39:00 +0200

    added hello.c

---
+++ Chapter2/hello.c
@@ -0,0 +1,16 @@
+/* 
+Name: exo1.c
+Purpose: Classic hello world program.
+Date: 09/20/2026 
+Author: Mehdi Fadel
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, world\n");
+    
+    return 0;  // Make the warning go away
+}
+    
